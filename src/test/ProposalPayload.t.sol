54c26818853d7f08415b39ee39e580c19038d2ec
Rajath Alex <rajathalex@gmail.com>
2022-09-27 17:32:52 -0400

    Updated tests according to latest Foundry features

--- src/test/ProposalPayload.t.sol
+++ src/test/ProposalPayload.t.sol
@@ -2,52 +2,35 @@
 pragma solidity ^0.8.15;
 
 // testing libraries
-import "@ds/test.sol";
-import "@std/console.sol";
-import {stdCheats} from "@std/stdlib.sol";
-import {Vm} from "@std/Vm.sol";
-import {DSTestPlus} from "@solmate/test/utils/DSTestPlus.sol";
+import "@forge-std/Test.sol";
 
 // contract dependencies
-import "../external/aave/IAaveGovernanceV2.sol";
-import "../external/aave/IExecutorWithTimelock.sol";
-import "../ProposalPayload.sol";
-import "../OneWayBondingCurve.sol";
-import "@openzeppelin/token/ERC20/IERC20.sol";
+import {IAaveGovernanceV2} from "../external/aave/IAaveGovernanceV2.sol";
+import {ProposalPayload} from "../ProposalPayload.sol";
+import {OneWayBondingCurve} from "../OneWayBondingCurve.sol";
+import {DeployMainnetProposal} from "../../script/DeployMainnetProposal.s.sol";
+import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
 
-contract ProposalPayloadTest is DSTestPlus, stdCheats {
-    Vm private vm = Vm(HEVM_ADDRESS);
-
+contract ProposalPayloadTest is Test {
     address public constant usdcTokenAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant ausdcTokenAddress = 0xBcca60bB61934080951369a648Fb03DF4F96263C;
     uint256 public constant usdcAmount = 603000e6;
     uint256 public constant ausdcAmount = 250000e6;
 
-    address public constant aaveEcosystemReserveController = 0x3d569673dAa0575c936c7c67c4E6AedA69CC630C;
-    address public constant aaveLendingPool = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
-    address public constant aaveGovernanceAddress = 0xEC568fffba86c094cf06b22134B23074DFE2252c;
-    address public constant aaveGovernanceShortExecutor = 0xEE56e2B3D491590B5b31738cC34d5232F378a8D5;
     address public constant aaveMainnetReserveFactor = 0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c;
 
-    IAaveGovernanceV2 private aaveGovernanceV2 = IAaveGovernanceV2(aaveGovernanceAddress);
-    IExecutorWithTimelock private shortExecutor = IExecutorWithTimelock(aaveGovernanceShortExecutor);
+    IAaveGovernanceV2 private aaveGovernanceV2 = IAaveGovernanceV2(0xEC568fffba86c094cf06b22134B23074DFE2252c);
 
     address[] private aaveWhales;
 
     address private proposalPayloadAddress;
-
-    address[] private targets;
-    uint256[] private values;
-    string[] private signatures;
-    bytes[] private calldatas;
-    bool[] private withDelegatecalls;
-    bytes32 private ipfsHash = 0x0;
 
     uint256 private proposalId;
 
     OneWayBondingCurve public oneWayBondingCurve;
 
     function setUp() public {
+        vm.createSelectFork(vm.rpcUrl("mainnet"), 15227480);
         // aave whales may need to be updated based on the block being used
         // these are sometimes exchange accounts or whale who move their funds
 
@@ -71,13 +54,14 @@ contract ProposalPayloadTest is DSTestPlus, stdCheats {
 
         vm.label(address(oneWayBondingCurve), "OneWayBondingCurve");
         vm.label(proposalPayloadAddress, "ProposalPayload");
-        vm.label(usdcTokenAddress, "usdcTokenAddress");
-        vm.label(ausdcTokenAddress, "ausdcTokenAddress");
-        vm.label(aaveEcosystemReserveController, "aaveEcosystemReserveController");
-        vm.label(aaveLendingPool, "aaveLendingPool");
-        vm.label(aaveMainnetReserveFactor, "aaveMainnetReserveFactor");
-        vm.label(aaveGovernanceAddress, "aaveGovernance");
-        vm.label(aaveGovernanceShortExecutor, "aaveGovernanceShortExecutor");
+    }
+
+    function testSetup() public {
+        IAaveGovernanceV2.ProposalWithoutVotes memory proposal = aaveGovernanceV2.getProposalById(proposalId);
+        assertEq(proposalPayloadAddress, proposal.targets[0], "TARGET_IS_NOT_PAYLOAD");
+
+        IAaveGovernanceV2.ProposalState state = aaveGovernanceV2.getProposalState(proposalId);
+        assertEq(uint256(state), uint256(IAaveGovernanceV2.ProposalState.Queued), "PROPOSAL_NOT_IN_EXPECTED_STATE");
     }
 
     function testExecute() public {
@@ -127,17 +111,11 @@ contract ProposalPayloadTest is DSTestPlus, stdCheats {
         ProposalPayload proposalPayload = new ProposalPayload(oneWayBondingCurve, usdcAmount, ausdcAmount);
         proposalPayloadAddress = address(proposalPayload);
 
-        bytes memory emptyBytes;
-
-        targets.push(proposalPayloadAddress);
-        values.push(0);
-        signatures.push("execute()");
-        calldatas.push(emptyBytes);
-        withDelegatecalls.push(true);
-
         vm.prank(aaveWhales[0]);
-        aaveGovernanceV2.create(shortExecutor, targets, values, signatures, calldatas, withDelegatecalls, ipfsHash);
-        proposalId = aaveGovernanceV2.getProposalsCount() - 1;
+        proposalId = DeployMainnetProposal._deployMainnetProposal(
+            proposalPayloadAddress,
+            0x344d3181f08b3186228b93bac0005a3a961238164b8b06cbb5f0428a9180b8a7 // TODO: Replace with actual IPFS Hash
+        );
     }
 
     /*******************************************************************************/
@@ -165,13 +143,5 @@ contract ProposalPayloadTest is DSTestPlus, stdCheats {
     function _skipQueuePeriod() public {
         IAaveGovernanceV2.ProposalWithoutVotes memory proposal = aaveGovernanceV2.getProposalById(proposalId);
         vm.warp(proposal.executionTime + 1);
-    }
-
-    function testSetup() public {
-        IAaveGovernanceV2.ProposalWithoutVotes memory proposal = aaveGovernanceV2.getProposalById(proposalId);
-        assertEq(proposalPayloadAddress, proposal.targets[0], "TARGET_IS_NOT_PAYLOAD");
-
-        IAaveGovernanceV2.ProposalState state = aaveGovernanceV2.getProposalState(proposalId);
-        assertEq(uint256(state), uint256(IAaveGovernanceV2.ProposalState.Queued), "PROPOSAL_NOT_IN_EXPECTED_STATE");
     }
 }
