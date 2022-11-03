6969319b8c6c199f13674ca92e3842abc4d40727
Rajath Alex <rajathalex@gmail.com>
2022-11-02 20:27:55 -0400

    Small fix

--- src/ProposalPayload.sol
+++ src/ProposalPayload.sol
@@ -18,7 +18,6 @@ contract ProposalPayload {
      *   CONSTANTS AND IMMUTABLES   *
      ********************************/
 
-    address public constant USDC_TOKEN = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant AUSDC_TOKEN = 0xBcca60bB61934080951369a648Fb03DF4F96263C;
 
     OneWayBondingCurve public immutable oneWayBondingCurve;
